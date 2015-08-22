1698f814a312f8f736e5873a71a875b953bae20a
omor <oscar@pradocapital.com>
2015-08-22 17:18:33 +0200

    discussions views working with image as attribute

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150822111707) do
+ActiveRecord::Schema.define(version: 20150822135938) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20150822111707) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "forem_categories", force: :cascade do |t|
